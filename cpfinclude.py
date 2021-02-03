4b74fbac27763b1139a3aef342575f318f3abba8
fatimasilvaed <fatimasilvaed@gmail.com>
2021-02-03 00:01:00 -0300

    alteração de inserção de cpf

--- cpfinclude.py
+++ cpfinclude.py
@@ -3,18 +3,16 @@ import json
 def addcpfs():
 
     #abrir arquivo Json para escrita
-    datacpfs = open("cpfs.json","w")
+    #datacpfs = open("cpfs.json","w")
 
     #inserir chave/valor arquivo json de cpfs
 
     print("___________CADASTRO DE CPFS___________\n")
     cpf = input("Insira CPF:\n")
-    tampletejson = {'cpf':'{}'.format(cpf)}
-    datacpfs.write(str(tampletejson))
-
-
-
-
+    tampletejson = '{"cpf":{}.'
+    tampletejson = (tampletejson)
+    cpfs = json.loads(tampletejson)
+    print(cpfs['cpf'])
 
 
 addcpfs()
