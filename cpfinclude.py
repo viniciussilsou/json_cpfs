783222e6222d06dc382b8ce9cbf48d3534e7c51e
fatimasilvaed <fatimasilvaed@gmail.com>
2021-02-04 22:29:32 -0300

    novo arquivo dividido por func

--- cpfinclude.py
+++ cpfinclude.py
@@ -1,17 +1,20 @@
 import json
 
-def addcpfs():
-    #abrindo arquivo json
-    datacpfs = open("cpfs.json", "w")
+def jsonopen(arquivo):
+   arquivo = open("cpfs.json","w")
 
-    #tamplate do objeto cpf
-    tampletecpfsjson = {}
-    #atribuindo valor a variavel cpf
-    cpf = input("Digite CPF:\n")
-    #definindo chave/valor ao objeto json colocando a variavel cpf como "valor"
-    tampletecpfsjson['cpf'] = cpf
-    json_cpfdata = json.dumps(tampletecpfsjson)
-    datacpfs.write(json_cpfdata)
+   return arquivo
+
+
+
+def addcpfs():
+
+    datacpfs = open("cpfs.json", "a")
+    cpfs = json.load(datacpfs)
+    cpf = int(input("Digite CPF:\n"))
+    cpfs.append(cpf)
+    cpfinsert = json.dumps(cpfs)
+    datacpfs.write(cpfinsert)
 
 
 addcpfs()
